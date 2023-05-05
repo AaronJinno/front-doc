fb811ad74c22ba06b3aa2061dfd86a9e62a2a5cf
AaronJinno <aaronjinno@gmail.com>
2023-05-05 15:51:00 +0000

    Deploying to docs from @ AaronJinno/front-doc@d369fe31714bd01a272a26f707b8ee6ade0d5e2a 🚀

---
+++ assets/3-8.其他.html-e721eb06.js
@@ -0,0 +1,8 @@
+import{_ as e,P as n,Q as i,a5 as s}from"./framework-3728cbf1.js";const a={},d=s(`<h4 id="gitignore" tabindex="-1"><a class="header-anchor" href="#gitignore" aria-hidden="true">#</a> .gitignore</h4><div class="language-.gitignore line-numbers-mode" data-ext=".gitignore"><pre class="language-.gitignore"><code>node_modules/
+.project
+unpackage/
+.DS_Store
+wxcomponents/**/*.vue
+wxcomponents/**/*.css
+.hbuilderx/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),r=[d];function c(t,l){return n(),i("div",null,r)}const u=e(a,[["render",c],["__file","3-8.其他.html.vue"]]);export{u as default};
